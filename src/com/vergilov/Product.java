f2ff1dad1b8a9f945fed6c137f034a42a6ea855a
Vergilov <biggohunter@gmail.com>
2018-09-15 21:43:36 +0200

    Adding formula to HealthyCalculator Adding new Class: Product

--- src/com/vergilov/Product.java
+++ src/com/vergilov/Product.java
@@ -5,4 +5,27 @@ public class Product {
     private double carbohydrantes;
     private double protein;
     private double fat;
+
+    public Product(String name, double carbohydrantes, double protein, double fat) {
+        this.name = name;
+        this.carbohydrantes = carbohydrantes;
+        this.protein = protein;
+        this.fat = fat;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getCarbohydrantes() {
+        return carbohydrantes;
+    }
+
+    public double getProtein() {
+        return protein;
+    }
+
+    public double getFat() {
+        return fat;
+    }
 }
